90097faed1c2abac0b72144617016e24136bf0ef
sarastatish <mutisomutinda23@gmail.com>
2024-03-04 23:27:20 +0300

    first commit

---
+++ usert_input.py
@@ -0,0 +1,4 @@
+name = input("what is your name:")
+age = input ("what is your age :")
+location = input ("where do youy live :")
+print ("hello "+ name  + "you are"+ age  + "and i live in " + location )
